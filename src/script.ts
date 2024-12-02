546f3f95eae669ee251c01e290a5291b6a3e141b
A91y <65825207+A91y@users.noreply.github.com>
2024-12-02 15:04:34 +0530

    added event listener and integrated Phala

---
+++ src/script.ts
@@ -0,0 +1,69 @@
+import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+// ABI of HabitDotV1 contract
+import contractABI from "./HabitDotV3.json";
+import { CONTRACT_ADDRESS } from "./constants";
+import axios from "axios";
+
+// Load environment variables
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rpc.api.moonbeam.network"
+); // RPC URL
+// const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL); // RPC URL
+const validatorPrivateKey = process.env.VALIDATOR_PRIVATE_KEY as string; // Validator private key
+const contractAddress = CONTRACT_ADDRESS; // Contract address
+
+// Validator wallet
+const validatorWallet = new ethers.Wallet(validatorPrivateKey, provider);
+
+// Contract instance
+const contract = new ethers.Contract(
+  contractAddress,
+  contractABI.abi,
+  validatorWallet
+);
+
+// Listen to `CheckInRequested` event
+async function listenForCheckInRequests(): Promise<void> {
+  console.log("Listening for CheckInRequested events...");
+
+  contract.on(
+    "CheckInRequested",
+    async (user: string, habitId: ethers.BigNumberish, proofUrl: string) => {
+      console.log(
+        `Check-in requested by user: ${user}, habit ID: ${habitId.toString()}, proof URL: ${proofUrl}`
+      );
+
+      try {
+        await fetchData(user, habitId.toString(), proofUrl);
+        console.log("Request Processed!");
+      } catch (error) {
+        console.error("Error processing:", error);
+      }
+    }
+  );
+}
+
+async function fetchData(user: string, habitId: string, proofUrl: string) {
+  const url = process.env.PHALA_URL as string;
+  const params = {
+    key: process.env.PHALA_KEY as string,
+    user: user,
+    habitId: habitId,
+    proofUrl:
+      "https://images.pexels.com/photos/1954524/pexels-photo-1954524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    model: "gpt-4o",
+  };
+
+  try {
+    const response = await axios.get(url, { params });
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+// Start the listener
+listenForCheckInRequests().catch(console.error);
